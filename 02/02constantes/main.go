abbe7c79d8a3a3c5c282caba5d8d3b3b8cc0e696
Alexys Lozada <alexyslozada@msn.com>
2016-11-30 14:18:51 -0500

    Clase 2 segunda parte

---
+++ 02/02constantes/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	//const nombre = "Pedro"
+	//fmt.Println(nombre)
+	/*
+		var a int
+		var b int8
+		n := "Pedro"
+		p := "Gómez"
+
+		a = 121212
+		b = 5
+
+		// casting
+		c := a + int(b)
+		fmt.Println(c)
+		fmt.Printf("hola %s %s %d cómo estás?\n", p, n, b)
+		fmt.Printf("b es de tipo: %T\n", b)
+	*/
+	// prioridad aritmética
+	// + - * /
+	// () % * / + -
+	// módulo es el residuo de la división.
+	// 7 % 3 = 1
+	//d := 6 + 6*(6-6)
+	//fmt.Println(d)
+
+	// Valor cero
+	// zero value
+	var nombre string
+	var numero int
+	var entiendes bool
+	fmt.Println(nombre, numero, entiendes)
+}
