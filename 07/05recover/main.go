875853ceada7d5900dc6bca53e1d96b51fad94b2
Daniel Romero <danielromeroauk@gmail.com>
2016-12-12 16:29:56 -0500

    Clase 07 parte 2.

---
+++ 07/05recover/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	f()
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("%T\n", r)
+			fmt.Println("Recuperado en f:", r)
+		}
+	}()
+
+	fmt.Println("Llamando g.")
+	g(5)
+	fmt.Println("Retorna normalmente desde g")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("AAAAAAAAaaaaaa")
+		panic(i)
+	}
+	defer fmt.Println("Defer en la función g")
+	fmt.Println("Imprimiendo en g", i)
+}
